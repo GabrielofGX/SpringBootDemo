e26a6aba4eb5753b51acdcfa79bae092da377fb6
gabriel <996492571@qq.com>
2018-12-13 15:59:43 +0800

    测试

---
+++ src/main/java/com/gabriel/dto/User.java
@@ -0,0 +1,17 @@
+package com.gabriel.dto;
+
+import lombok.Data;
+
+/**
+ * @author gabriel
+ * @Type User
+ * @Desc
+ * @date 2018/12/13 15:02
+ */
+@Data
+public class User {
+
+	private Integer id;
+	private String name;
+	private Integer age;
+}
